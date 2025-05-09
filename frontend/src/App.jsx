fa359c3880dbe97ce421b49973fcfc543a0ebe1f
Satyam Singh <satyamrajput7239@gmail.com>
2025-05-09 06:37:45 +0530

    feat: add routing and api in F.E

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,35 @@
-import "./App.css";
-// import LoginRegister from "./pages/LoginRegister";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
+import LoginRegister from "./pages/LoginRegister";
 import TaskManager from "./pages/TaskManager";
+import "./App.css";
 function App() {
   return (
-    <>
-      <TaskManager />
-      {/* <LoginRegister /> */}
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <LoginRegister
+            // isAdmin={isAdmin}
+            // isValid={isValid}
+            // username={username}
+            />
+          }
+        ></Route>
+        <Route
+          path="/tasks"
+          element={
+            <TaskManager
+            // isAdmin={isAdmin}
+            // isValid={isValid}
+            // username={username}
+            />
+          }
+        ></Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/utils/apis.js
@@ -0,0 +1,32 @@
+const API_URL = "http://localhost:8080";
+export const loginUser = async (email, password) => {
+  const res = await fetch(`${API_URL}/api/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  const resJson = await res.json();
+  localStorage.setItem("token", resJson.token);
+  return resJson;
+};
+
+export const registerUser = async (email, password) => {
+  const res = await fetch(`${API_URL}/api/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  if (res.ok) {
+    const resJson = await res.json();
+    localStorage.setItem("token", resJson.token);
+    return resJson;
+  }
+  return { error: true };
+};
+export const verifyToken = async (token) => {
+  const res = await fetch(`${API_URL}/api/auth/verify`, {
+    method: "GET",
+    headers: { Authorization: token },
+  });
+  return res.json();
+};
